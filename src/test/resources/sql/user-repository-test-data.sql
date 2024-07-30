3fe7379bb00d713e9c9fde4f9c8fb89798517f23
jaein-Jang <jjjwodls@gmail.com>
2024-07-30 16:40:04 +0900

    init commit and 사용하지 않는 메서드 삭제 및 네이밍 변경

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into users (id, address, certification_code, email, last_login_at, nickname, status)
+values ('1', '서울','aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'test@gmail.com', '0', 'nickname', 'ACTIVE');
